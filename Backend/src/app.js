1b3f3bc7f1253c18acce3bb5934ab80b67bf4a85
Gourav Manjhi <gouravmanjhi9313@gmail.com>
2026-03-06 16:34:55 +0530

    allow vercel frontend cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -51,6 +51,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
+      "https://tutroid.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
